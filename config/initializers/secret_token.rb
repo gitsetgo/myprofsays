36ca524098f7509ec802cae3f719fb54889c9eb8
Ajay Ravindra <gitsetgo@gmail.com>
2012-03-29 12:47:45 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myprofsays::Application.config.secret_token = 'cd3d0616766223c4a64f266daef1afdbce739d640c510850591635cd2c91aaca680e4abf5910aea905aa30974b47ac80e745ac5af2153b274a1e88d198987dac'
